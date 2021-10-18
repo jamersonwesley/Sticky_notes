1128d2049cf2f86dce37f881d57dcd2075caa2d3
jamersonwesley <jamerson_wesley@hotmail.com>
2021-10-18 13:38:51 -0300

    18/10/2021

--- index.php
+++ index.php
@@ -8,10 +8,37 @@
     <title>Document</title>
 </head>
 <body>
-    <nav class="topo"></nav>
-    <div class="conteudo"></div>
+    <nav class="topo"><img src="./images/User.png" alt="" width="50px" height="50px"><a>User icon</nav>
+    <div class="conteudo">
+        <div class="anotacoes">
+            <div class="anotacao-single">
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Sua nova anotação"></textarea>
+            </div>
+          
+        </div>
+        <div class="adicionar">
+    <p>+</p>
+        </div></div>
     <footer class="rodape"></footer>
 
 
 </body>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.js"
+  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  crossorigin="anonymous"></script>
+  <script type="text/javascript">
+  var i= 0 ;
+          $(function () {
+                $('.adicionar').click(function(){
+                    
+                     if(i<8){
+                        var el = '<div class="anotacao-single"><textarea name="" id="" cols="30" rows="10" placeholder="Sua nova anotação"></textarea></div>';
+                        $('.anotacoes').append(el);
+                        i++;
+                  }else{
+                      alert("Numero de anotações atingido no maximo");
+                  }  })
+          })
+  </script>
 </html>
